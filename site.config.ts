43146450f084333244a90a791ea1413c942adc88
于见怪不怪 <yjgbg1239461052@gmail.com>
2022-10-01 00:43:44 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: '于见怪不怪',
+  github: 'yjgbg',
   linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
